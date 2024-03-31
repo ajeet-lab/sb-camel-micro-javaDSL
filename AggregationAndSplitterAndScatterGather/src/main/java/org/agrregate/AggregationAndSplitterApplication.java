ee0658fd3057484b01c3398f874aceae80f8b443
AJEET KUSHWAHA <51816732+ajeet-lab@users.noreply.github.com>
2024-03-31 20:17:20 +0530

    update code

--- AggregationAndSplitterAndScatterGather/src/main/java/org/agrregate/AggregationAndSplitterApplication.java
+++ AggregationAndSplitterAndScatterGather/src/main/java/org/agrregate/AggregationAndSplitterApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AggregationAndSplitterApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(AggregationAndSplitterApplication.class, args);
     }
-
 }
---
+++ AggregationAndSplitterAndScatterGather/src/main/java/org/agrregate/ScatterAndGatherMulticastRoute.java
@@ -0,0 +1,38 @@
+package org.agrregate;
+
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ScatterAndGatherMulticastRoute extends RouteBuilder {
+    @Override
+    public void configure() throws Exception {
+
+        rest("/api").consumes(MediaType.APPLICATION_JSON_VALUE).produces(MediaType.APPLICATION_JSON_VALUE)
+                .post("/posts2")
+                .route()
+                .removeHeaders("*")
+                .multicast(new HttpAggregateStretegy()).parallelProcessing()
+                .to("direct:endpoint1", "direct:endpoint2", "direct:endpoint3")
+                .end();
+
+        from("direct:endpoint1")
+                .setHeader("CamelHttpMethod", constant("GET"))
+                .toD("https://jsonplaceholder.typicode.com/posts/1?bridgeEndpoint=true&throwExceptionOnFailure=false")
+                .convertBodyTo(String.class)
+         ;
+
+        from("direct:endpoint2")
+                .setHeader("CamelHttpMethod", constant("GET"))
+                .toD("https://jsonplaceholder.typicode.com/posts/2?bridgeEndpoint=true&throwExceptionOnFailure=false")
+                .convertBodyTo(String.class);
+
+        from("direct:endpoint3")
+                .setHeader("CamelHttpMethod", constant("GET"))
+                .toD("https://jsonplaceholder.typicode.com/posts/3?bridgeEndpoint=true&throwExceptionOnFailure=false")
+                .convertBodyTo(String.class);
+    }
+
+}
